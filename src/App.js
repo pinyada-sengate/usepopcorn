ce64e9831a3fd8ae68cfa9b5248e03d30101d158
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-02-09 13:36:20 +0100

    clean up data fetching

--- src/App.js
+++ src/App.js
@@ -79,12 +79,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController(); //brower API
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) {
@@ -97,8 +100,11 @@ export default function App() {
           }
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -111,6 +117,11 @@ export default function App() {
       }
 
       fetchMovies();
+
+      // clen up fetch
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
